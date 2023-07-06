51b6bab21932a162b7aaad24f34cf6d33662b66b
bbradz <benjamin_bradley@brown.edu>
2023-07-06 19:07:21 -0400

    Turned ExcelExtracting.py into a script and called it within interact.py in order to start the process of being able to add all the info into Postgres database, I just need to figure out how to add day by day and then I can go through each of the days in order to add it day by day

--- ExcelExtracting.py
+++ ExcelExtracting.py
@@ -1,92 +1,93 @@
 from openpyxl import load_workbook
 import re
 
-data_file = '/Users/benbradley/Downloads/Current Time Tracking 🗓️.xlsx'
+def extract():
+    data_file = '/Users/benbradley/Downloads/Current Time Tracking 🗓️.xlsx'
 
-# Load the entire workbook.
-wb = load_workbook(data_file)
+    # Load the entire workbook.
+    wb = load_workbook(data_file)
 
-# List all the sheets in the file.
-sheetNames = ['June-23','Month Nav','Week Template','Dropdown Backend','Ideas...','Jan-23','Feb-23','Mar-23','Apr-23','May-23','To SQL']
+    # List all the sheets in the file.
+    sheetNames = ['June-23','Month Nav','Week Template','Dropdown Backend','Ideas...','Jan-23','Feb-23','Mar-23','Apr-23','May-23','To SQL']
 
-# Lists to be filled with 30-minute swaths
-jun23 = [0]*4
-jan23 = [0]*4
-feb23 = [0]*4
-mar23 = [0]*4
-apr23 = [0]*4
-may23 = [0]*4
+    # [Category, DeltaMoney, Who?, Details]
+    # Lists to be filled with 30-minute swaths
+    jun23 = [0]*4
+    jan23 = [0]*4
+    feb23 = [0]*4
+    mar23 = [0]*4
+    apr23 = [0]*4
+    may23 = [0]*4
 
-# Lists to be filled with day info
-# [👕,🧼,💩,🍺,🍃,Caffeine,Work,💤]
-days_info = [0]*8
+    # Lists to be filled with day info
+    # [👕,🧼,💩,🍺,🍃,Caffeine,Work,💤]
+    days_info = [0]*8
 
-# Put together list of possible Categories & Group types
-ws = wb['Dropdown Backend']
-all_cols = list(ws.columns)
-options = []
-for col in all_cols:
-    for cell in col:
-        if cell.value != None:
-            options.append(cell.value)
+    # Put together list of possible Categories & Group types
+    ws = wb['Dropdown Backend']
+    all_cols = list(ws.columns)
+    options = []
+    for col in all_cols:
+        for cell in col:
+            if cell.value != None:
+                options.append(cell.value)
 
-categories = options[1:31]
-groups = options[32:]
+    categories = options[1:31]
+    groups = options[32:]
 
-def process_col(month, col, min, max, index):
-    output = []
-    for cell in col:
-        if max > 0 & min < 1:
-            output.append(cell.value)
-            max -= 1
-            min -= 1
-    month[index] = output
+    def process_col(month, col, min, max, index):
+        output = []
+        for cell in col:
+            if max > 0 & min < 1:
+                output.append(cell.value)
+                max -= 1
+                min -= 1
+        month[index] = output
 
-all_contents = []
+    # all_contents = []
 
-# Process primary 30-minute swaths for every month from 'To SQL' up to end of 11-6-23
-ws = wb['To SQL']
-all_cols = list(ws.columns)
-x = 0
-for col in all_cols:
-    if (x==0 or x==1 or x==2 or x==3):
-        process_col(jan23, col, 0, 1200, x%5)
-    if (x==5 or x==6 or x==7 or x==8):
-        process_col(feb23, col, 0, 1344, x%5)
-    if (x==10 or x==11 or x==12 or x==13): 
-        process_col(mar23, col, 0, 1488, x%5)
-    if (x==15 or x==16 or x==17 or x==18):
-        process_col(apr23, col, 0, 1440, x%5)
-    if (x==20 or x==21 or x==22 or x==23):
-        process_col(may23, col, 0, 1488, x%5) 
-    if (x==25 or x==26 or x==27 or x==28):
-        process_col(jun23, col, 0, 1440, x%5)
-    x += 1
-    
-# Process additional info collected daily from 'Month Nav' up to end of May
-ws = wb['Month Nav']
-all_cols = list(ws.columns)
-x = 0
-for col in all_cols:
-    if x==83:
-        process_col(days_info, col, 8, 178, 6)
-    if x==84:
-        process_col(days_info, col, 8, 178, 0)
-    if x==85:
-        process_col(days_info, col, 8, 178, 1)
-    if x==86:
-        process_col(days_info, col, 8, 178, 2)
-    if x==87:
-        process_col(days_info, col, 8, 178, 3)
-    if x==88:
-        process_col(days_info, col, 8, 178, 4)
-    if x==89:
-        process_col(days_info, col, 8, 178, 5)
-    if x==90:
-        process_col(days_info, col, 33, 178, 7)
-    x += 1
+    # Process primary 30-minute swaths for every month from 'To SQL' up to end of 11-6-23
+    ws = wb['To SQL']
+    all_cols = list(ws.columns)
+    x = 0
+    for col in all_cols:
+        if (x==0 or x==1 or x==2 or x==3):
+            process_col(jan23, col, 0, 1200, x%5)
+        if (x==5 or x==6 or x==7 or x==8):
+            process_col(feb23, col, 0, 1344, x%5)
+        if (x==10 or x==11 or x==12 or x==13): 
+            process_col(mar23, col, 0, 1488, x%5)
+        if (x==15 or x==16 or x==17 or x==18):
+            process_col(apr23, col, 0, 1440, x%5)
+        if (x==20 or x==21 or x==22 or x==23):
+            process_col(may23, col, 0, 1488, x%5) 
+        if (x==25 or x==26 or x==27 or x==28):
+            process_col(jun23, col, 0, 1440, x%5)
+        x += 1
+        
+    # Process additional info collected daily from 'Month Nav' up to end of May
+    ws = wb['Month Nav']
+    all_cols = list(ws.columns)
+    x = 0
+    for col in all_cols:
+        if x==83:
+            process_col(days_info, col, 8, 178, 6)
+        if x==84:
+            process_col(days_info, col, 8, 178, 0)
+        if x==85:
+            process_col(days_info, col, 8, 178, 1)
+        if x==86:
+            process_col(days_info, col, 8, 178, 2)
+        if x==87:
+            process_col(days_info, col, 8, 178, 3)
+        if x==88:
+            process_col(days_info, col, 8, 178, 4)
+        if x==89:
+            process_col(days_info, col, 8, 178, 5)
+        if x==90:
+            process_col(days_info, col, 33, 178, 7)
+        x += 1
 
-# Still need to add in Pages Read + Bed time
-
-# print(all_contents)
-# re.findall(r'([1-9]*[-][1-9]*[-][1-9]*)\w', all_contents)
+    # Still need to add in Pages Read + Bed time
+    # re.findall(r'([1-9]*[-][1-9]*[-][1-9]*)\w', all_contents)
+    return jan23, feb23, mar23, apr23, may23, jun23, days_info
--- interact.py
+++ interact.py
@@ -7,27 +7,37 @@ create table dailyData (t1 Date, t2 Day, sleep float DEFAULT 0, workTime float D
 import psycopg2;
 import psycopg2.extras;
 import creds;
+import ExcelExtracting;
+creds = creds.creds
 
-# Run command and commit to db
+#   Run command and commit to db
 def run(message):
     cur.execute(message)
     conn.commit()
-
 #   print out contents SELECTed
 def display(message):
     cur.execute(message)
     print(cur.fetchall())
 
-creds = creds.creds
+#   Call ExcelExtracting and assign it to new variables
+jan23, feb23, mar23, apr23, may23, jun23, days_info = ExcelExtracting.extract()
 
-conn = psycopg2.connect(dbname=creds["dbname"], 
-                 user=creds["user"], 
-                 password=creds["password"], 
-                 host=creds["host"])
+try:
+    conn = psycopg2.connect(dbname=creds["dbname"], 
+                    user=creds["user"], 
+                    password=creds["password"], 
+                    host=creds["host"])
+    cur = conn.cursor()
 
-cur = conn.cursor()
+    # run("create table granularData (t1 Date, t2 VARCHAR(10), t3 Time, timezone VARCHAR(5), category VARCHAR(255), deltaMoney money, audience VARCHAR(255), descr VARCHAR(8000));")
+    # run("create table dailyData (t1 Date, t2 VARCHAR(10), sleep float DEFAULT 0, workTime float DEFAULT 0, laundry int DEFAULT 0, shower int DEFAULT 0, poop int DEFAULT 0, a int DEFAULT 0, w int DEFAULT 0, summary VARCHAR(8000));")
 
-run("create table granularData (t1 Date, t2 VARCHAR(10), t3 Time, timezone VARCHAR(5), category VARCHAR(255), deltaMoney money, audience VARCHAR(255), descr VARCHAR(8000));")
-run("create table dailyData (t1 Date, t2 VARCHAR(10), sleep float DEFAULT 0, workTime float DEFAULT 0, laundry int DEFAULT 0, shower int DEFAULT 0, poop int DEFAULT 0, a int DEFAULT 0, w int DEFAULT 0, summary VARCHAR(8000));")
+    
 
-conn.close()
+except Exception as error:
+    print(error)
+finally:
+    if cur is not None:
+        cur.close()
+    if conn is not None:
+        conn.close()
